75f001af8354ac4fea690d9050dae30800cf6178
Lusine Am <lusine.a.amiraghyan@gmail.com>
2021-02-03 16:40:48 +0400

    creating items list project

---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ListItem (){
+    return <div>
+        <h2>Apple</h2>
+        
+        <p>Green Aplle</p>
+        <p>Price 1 $</p>
+        <button>Buy</button>
+    </div>
+}
+
+export default ListItem;
---
+++ src/containers/List/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListItem from '../../components/ListItem/ListItem';
+
+function List () {
+    return <div>
+        <ListItem />
+        <ListItem />
+        <ListItem />
+    </div>
+}
+export default List;
